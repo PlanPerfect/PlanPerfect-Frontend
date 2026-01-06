78f3266ea185f85549c0542dac2f6b7e35612dc9
lincoln0623 <lincolnlim267@gmail.com>
2026-01-06 21:42:09 +0800

    Added networking.js

--- src/pages/Sample.jsx
+++ src/pages/Sample.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import { Container, Stack, Heading, Text, Button, HStack, Badge, Box } from '@chakra-ui/react'
 import ShowToast from "../Extensions/ShowToast"
+import server from "../../networking"
 
-function Homepage() {
+function Sample() {
     return (
         <Container maxW="4xl" py={{ base: "12", md: "24" }}>
             <Stack gap="8" align="center" textAlign="center">
@@ -37,10 +38,30 @@ function Homepage() {
                     >
                         Did someone say toast?
                     </Button>
+                    <Button
+                        colorPalette={"green"}
+                        onClick={async () => {
+                            try {
+                            const data = await server.get("/sample/get-data");
+                            if (data.data.status === 200 && data.data.success == true) {
+                                console.log("Networking successful:", data.data);
+                                ShowToast("success", "Networking successful", "Check console for details");
+                            } else {
+                                console.log("Networking failed with status:", data.data.status);
+                                ShowToast("error", "Networking failed", "Check console for details");
+                            }
+                            } catch (error) {
+                                console.error("Networking error:", error);
+                                ShowToast("error", "Networking error", "Check console for details");
+                            }
+                        }}
+                    >
+                        Networking Test Button!
+                    </Button>
                 </HStack>
             </Stack>
         </Container>
     )
 }
 
-export default Homepage
+export default Sample
