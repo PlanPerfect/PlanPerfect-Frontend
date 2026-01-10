c17dce3aec4b112db67855f04e7b4de9b6337e48
lincoln0623 <lincolnlim267@gmail.com>
2026-01-11 03:03:50 +0800

    Styled hero section.

--- src/pages/newHomeOwners/NewHomeOwnerPage.jsx
+++ src/pages/newHomeOwners/NewHomeOwnerPage.jsx
@@ -4,29 +4,15 @@ function NewHomeOwnerPage() {
 	return (
 		<>
 			{/* Hero Section */}
-			<Box
-				bgImage="url('/newHomeOwnerHero.png')"
-				bgRepeat="no-repeat"
-				bgSize="cover"
-				bgPosition="center"
-				minH="100vh"
-			>
-				<Flex
-					direction="column"
-					justify="center"
-					align="center"
-					textAlign="center"
-					h="100%"
-					color="white"
-					px={6}
-				>
-					<Heading size="6xl" mb={4}>
+			<Box bgImage="url('/newHomeOwnerHero.png')" bgRepeat="no-repeat" bgSize="cover" bgPosition="center" minH="100vh" display="flex" alignItems="center" justifyContent="center">
+				<Flex direction="column" justify="center" align="center" textAlign="center" h="100%" px={6}>
+					<Heading fontSize="80px" mb={4} color="white">
 						Let Our AI Agent Find Your
 					</Heading>
-					<Heading size="6xl" mb={4}>
-						Perfect Design Style!
-					</Heading>
-					<Text fontSize="lg" mb={6} maxW="700px">
+                    <Heading fontSize="80px" mb={4} lineHeight="1.5" bgGradient="to-r" gradientFrom="#F4E5B2" gradientTo="#D4AF37" bgClip="text" color="transparent">
+                        Perfect Design Style!
+                    </Heading>
+					<Text fontSize="2xl" mt={8} mb={6} maxW="700px" color="white">
 						Complete the 5 steps below to get your personalized design style guide and mood board now!
 					</Text>
 				</Flex>
