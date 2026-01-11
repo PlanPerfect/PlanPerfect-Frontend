86a8f75b1189b72a866c899a638292f324ee495e
Joshua Long Yu Xuan <joshu5739yx@gmail.com>
2026-01-12 01:03:35 +0800

    fixed rendering issue in navbar by applying display styles globally at layout level

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -6,11 +6,39 @@ import Navbar from './components/Navbar'
 
 function App() {
 	return (
-		<div className='defaultLayout'>
-			<Navbar />
-			<Outlet />
-            <Toaster />
-		</div>
+		<>
+			<div className='defaultLayout'>
+				<Navbar />
+				<Outlet />
+				<Toaster />
+			</div>
+
+			<style>
+                {`
+                    @keyframes fadeInDown {
+                        from {
+                            opacity: 0;
+                            transform: translateY(-20px);
+                        }
+                        to {
+                            opacity: 1;
+                            transform: translateY(0);
+                        }
+                    }
+
+                    @keyframes fadeInUp {
+                        from {
+                            opacity: 0;
+                            transform: translateY(20px);
+                        }
+                        to {
+                            opacity: 1;
+                            transform: translateY(0);
+                        }
+                    }
+                `}
+            </style>
+		</>
 	)
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { Provider } from "@/components/ui/provider"
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import GetStarted from './pages/stylematch/GetStarted.jsx'
 import Layout from './Layout.jsx'
 import Homepage from "./pages/Homepage.jsx"
 import './index.css'
@@ -11,6 +12,10 @@ createRoot(document.getElementById('root')).render(
 			<Routes>
 				<Route path={"/"} element={<Layout />} >
 					<Route index element={<Homepage />} />
+
+					<Route path={"stylematch"}>
+						<Route index element={<GetStarted />} />
+					</Route>
 				</Route>
 			</Routes>
 		</BrowserRouter>
