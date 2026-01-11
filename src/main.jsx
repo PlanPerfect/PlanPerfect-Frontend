6c37cdbffde241de54026999c8e25a46bffcefbb
Joshua Long Yu Xuan <joshu5739yx@gmail.com>
2026-01-12 01:08:59 +0800

    added base routes for services

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,10 @@
 import { Provider } from "@/components/ui/provider"
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import GetStarted from './pages/stylematch/GetStarted.jsx'
 import Layout from './Layout.jsx'
 import Homepage from "./pages/Homepage.jsx"
+import Services from './pages/Onboarding/Services.jsx'
+import GetStarted from './pages/StyleMatch/GetStarted.jsx'
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
@@ -12,6 +13,9 @@ createRoot(document.getElementById('root')).render(
 			<Routes>
 				<Route path={"/"} element={<Layout />} >
 					<Route index element={<Homepage />} />
+					<Route path={"onboarding"}>
+						<Route index element={<Services />} />
+					</Route>
 
 					<Route path={"stylematch"}>
 						<Route index element={<GetStarted />} />
