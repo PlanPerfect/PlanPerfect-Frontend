bec0196dbf82ba1528a6751642459cc53e920d0d
Joshua Long Yu Xuan <joshu5739yx@gmail.com>
2026-01-20 14:18:20 +0800

    changed route path

--- src/main.jsx
+++ src/main.jsx
@@ -36,8 +36,8 @@ createRoot(document.getElementById('root')).render(
 						<Route index element={<ExistingHomeOwner />} />
 					</Route>
 
-					<Route path={"lumen"}>
-						<Route index element={<ChatbotPage />} />
+					<Route path={"chat"}>
+						<Route path={"lumen"} element={<ChatbotPage />} />
 					</Route>
 				</Route>
 			</Routes>
